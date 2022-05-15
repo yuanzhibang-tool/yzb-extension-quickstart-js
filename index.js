344e1bf8ab93ba809a7cb1206cdb93bbbfbe9b61
shandawei <shandawei@me.com>
2022-05-16 01:43:09 +0800

    优化调用

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const yzbNode = require('@yuanzhibang/node');
-
+// import { ipc } from "@yuanzhibang/node"
 console.log("Hello from child process");
-
+const { ipc } = require("@yuanzhibang/node");
 // 接收渲染端发送的消息
-yzbNode.ipc.on('test-node-message-topic', (sender, message) => {
+ipc.on('test-node-message-topic', (sender, message) => {
     console.log(message);
     sender.error(message);
     // sender.next(message);
-    yzbNode.ipc.send('test-renderer-message-topic', { test: 'test-renderer-message', test1: 'test1' });
+    ipc.send('test-renderer-message-topic', { test: 'test-renderer-message', test1: 'test1' });
 });
 
